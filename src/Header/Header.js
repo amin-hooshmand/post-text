9b1d7dca4c2cb140311c71a561f8ef6ffd923285
amin-hooshmand <aminhoushmand78@gmail.com>
2022-06-26 18:56:06 +0430

    Add Components

---
+++ src/Header/Header.js
@@ -0,0 +1,13 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div>
+        <p>اسم و ایمیل خود را ثبت کنید</p>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/Main/Main.js
@@ -0,0 +1,17 @@
+const Main = () => {
+  return (
+    <form>
+      <div>
+        <label>اسم خود را وارد کنید</label>
+        <input type="text" />
+      </div>
+
+      <div>
+        <label>ایمیل خود را وارد کنید</label>
+        <input type="email" />
+      </div>
+    </form>
+  );
+};
+
+export default Main;
